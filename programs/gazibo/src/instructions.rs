f41ea8b941e8fe7ce1af1c8eda34810aa5a32a2a
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-06-28 10:11:51 +0000

    use targeted name re-exports to prevente handler collision accross modules

--- programs/gazibo/src/instructions.rs
+++ programs/gazibo/src/instructions.rs
@@ -1,6 +1,9 @@
 pub mod accept_job;
 pub mod create_job;
 pub mod deliver_job;
-pub mod initialize;
+pub mod release_payment;
 
-pub use initialize::*;
+pub use accept_job::AcceptJob;
+pub use create_job::CreateJob;
+pub use deliver_job::DeliverJob;
+pub use release_payment::ReleasePayment;
