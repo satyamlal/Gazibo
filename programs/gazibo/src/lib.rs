03a441a45ba811f6bf688fdaeef6ebe3cfcd136a
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-06-28 10:03:42 +0000

    update these create_job() accept_job() deliver_job() release_payment()

--- programs/gazibo/src/lib.rs
+++ programs/gazibo/src/lib.rs
@@ -1,9 +1,9 @@
+use anchor_lang::prelude::*;
+
 pub mod constants;
 pub mod error;
 pub mod instructions;
 pub mod state;
-
-use anchor_lang::prelude::*;
 
 pub use constants::*;
 pub use instructions::*;
@@ -15,7 +15,24 @@ declare_id!("5bTvxVfSwqy7FApuPfHiXBWihZSmLSQNsGwVebNeLj7D");
 pub mod gazibo {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn create_job(
+        ctx: Context<CreateJob>,
+        title: String,
+        amount: u64,
+        job_id: u64,
+    ) -> Result<()> {
+        instructions::create_job::handler(ctx, title, amount, job_id)
+    }
+
+    pub fn accept_job(ctx: Context<AcceptJob>) -> Result<()> {
+        instructions::accept_job::handler(ctx)
+    }
+
+    pub fn deliver_job(ctx: Context<DeliverJob>) -> Result<()> {
+        instructions::deliver_job::handler(ctx)
+    }
+
+    pub fn release_payment(ctx: Context<ReleasePayment>) -> Result<()> {
+        instructions::release_payment::handler(ctx)
     }
 }
