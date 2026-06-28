fc4f078445fba210beae4c0fd977d80eb16a76c9
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-06-28 09:57:50 +0000

    update error codes

--- programs/gazibo/src/error.rs
+++ programs/gazibo/src/error.rs
@@ -1,7 +1,37 @@
 use anchor_lang::prelude::*;
 
 #[error_code]
-pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+pub enum GaziboError {
+    #[msg("Amount must be greater than zero!")]
+    InvalidAmount,
+
+    #[msg("Title can't be empty!")]
+    TitleEmpty,
+
+    #[msg("Title is too long, max 50 characters allowed!")]
+    TitleTooLong,
+
+    #[msg("Job is not open for applications!")]
+    JobNotOpen,
+
+    #[msg("Job is not currently in progress!")]
+    JobNotInProgress,
+
+    #[msg("Job has not been marked as Delivered yet!")]
+    JobNotDelivered,
+
+    #[msg("Only the assigned freelancer can do this action!")]
+    UnauthorizedFreelancer,
+
+    #[msg("Only the client who creatd this job can perform this action!")]
+    UnauthorizedClient,
+
+    #[msg("No freelancer assigned to this job!")]
+    NoFreelancerAssigned,
+
+    #[msg("The client can't be the freelancer on their own job!")]
+    ClientCannotBeFreelancer,
+
+    #[msg("Escrow accounts has insufficient lamports!")]
+    InsufficientEscrowFunds,
 }
