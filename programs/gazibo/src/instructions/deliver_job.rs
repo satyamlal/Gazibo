9cacd9fc9fff4c8017cf16129d7fdc46a2d1d698
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-06-30 18:54:32 +0000

    update the deliver_job_handler()

--- programs/gazibo/src/instructions/deliver_job.rs
+++ programs/gazibo/src/instructions/deliver_job.rs
@@ -1,11 +1,40 @@
+use crate::constants::*;
+use crate::error::GaziboError;
+use crate::state::{Job, JobStatus};
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
 pub struct DeliverJob<'info> {
     #[account(mut)]
     pub client: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            JOB_SEED,
+            job.client.as_ref(),
+            &job.job_id.to_le_bytes(),
+        ],
+        bump = job.bump,
+    )]
+    pub job: Account<'info, Job>,
 }
 
-pub fn deliver_job_handler(_ctx: Context<DeliverJob>) -> Result<()> {
+pub fn deliver_job_handler(ctx: Context<DeliverJob>) -> Result<()> {
+    let job = &mut ctx.accounts.job;
+    let freelancer = ctx.accounts.freelancer.key();
+
+    require!(
+        job.status == JobStatus::InProgress,
+        GaziboError::JobNotInProgress,
+    );
+    require!(job.freelancer.is_some(), GaziboError::NoFreelancerAssigned);
+    require!(
+        job.freelancer == Some(freelancer),
+        GaziboError::UnauthorizedFreelancer
+    );
+
+    job.status = JobStatus::Delivered;
+
     Ok(())
 }
