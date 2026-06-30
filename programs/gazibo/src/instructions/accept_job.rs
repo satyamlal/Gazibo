284509cff5d622aaeb110fb64873719d0119810e
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-06-30 19:44:50 +0000

    add job Account update accept_job_handler()

--- programs/gazibo/src/instructions/accept_job.rs
+++ programs/gazibo/src/instructions/accept_job.rs
@@ -1,11 +1,37 @@
+use crate::constants::*;
+use crate::error::GaziboError;
+use crate::state::{Job, JobStatus};
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
 pub struct AcceptJob<'info> {
     #[account(mut)]
-    pub client: Signer<'info>,
+    pub freelancer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            JOB_SEED,
+            job.client.as_ref(),
+            &job.job_id.to_le_bytes(),
+        ],
+        bump = job.bump
+    )]
+    pub job: Account<'info, Job>,
 }
 
-pub fn accept_job_handler(_ctx: Context<AcceptJob>) -> Result<()> {
+pub fn accept_job_handler(ctx: Context<AcceptJob>) -> Result<()> {
+    let job = &mut ctx.accounts.job;
+    let freelancer = ctx.accounts.freelancer.key();
+
+    require!(job.status == JobStatus::Open, GaziboError::JobNotOpen);
+    require!(
+        job.client != freelancer,
+        GaziboError::ClientCannotBeFreelancer,
+    );
+
+    job.freelancer = Some(freelancer);
+    job.status = JobStatus::InProgress;
+
     Ok(())
 }
