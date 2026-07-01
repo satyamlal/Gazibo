b3c925c417d8cfa23fc9182202b0bf0067f8061d
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-07-01 08:20:04 +0000

    add new constants: MAX_DESC_LENGTH MIN_AMOUNT_LAMPORT = 1_000_000

--- programs/gazibo/src/constants.rs
+++ programs/gazibo/src/constants.rs
@@ -3,3 +3,6 @@ use anchor_lang::prelude::*;
 #[constant]
 pub const JOB_SEED: &[u8] = b"job";
 pub const MAX_TITLE_LENGTH: usize = 50;
+pub const MAX_DESC_LENGTH: usize = 500;
+
+pub const MIN_AMOUNT_LAMPORTS: u64 = 1_000_000; //0.001 SOL = 1_000_000 lamports
