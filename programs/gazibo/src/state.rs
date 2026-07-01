fd5e3cd48f425ce3c673f2b1b0683ae5e8b2e05a
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-07-01 10:23:10 +0000

    update state structs: add new fields to job struct impl Job to calculate actual string length not MAX lengths add new fields in JobStatus enum impl default for JobStatus enum

--- programs/gazibo/src/state.rs
+++ programs/gazibo/src/state.rs
@@ -10,8 +10,28 @@ pub struct Job {
 
     #[max_len(50)]
     pub title: String,
-    pub bump: u8,
+
+    #[max_len(500)]
+    pub description: String,
+
     pub job_id: u64,
+    pub create_at: i64,
+    pub bump: u8,
+}
+
+impl Job {
+    pub fn space(title_len: usize, desc_len: usize) -> usize {
+        8 + // Anchor discriminator (always first, always 8 bytes)
+        32 + // client: Pubkey
+        33 + // freelancer: Option<Pubkey>
+        8 + // amount
+        1 + // 
+        8 + // job_id: u64
+        (4 + title_len) + // title: String -> 4 bytes 
+        (4 + desc_len) + // description: String -> 4 bytes
+        8 + // created_at: u64
+        1 //bump
+    }
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq, InitSpace)]
@@ -20,4 +40,11 @@ pub enum JobStatus {
     InProgress,
     Delivered,
     Completed,
+    Cancelled,
+}
+
+impl Default for JobStatus {
+    fn default() -> Self {
+        JobStatus::Open
+    }
 }
