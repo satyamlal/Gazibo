1263918668c57acf76611fcf48934c86441dcd36
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-07-01 08:27:30 +0000

    update error messages

--- programs/gazibo/src/error.rs
+++ programs/gazibo/src/error.rs
@@ -2,15 +2,6 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum GaziboError {
-    #[msg("Amount must be greater than zero!")]
-    InvalidAmount,
-
-    #[msg("Title can't be empty!")]
-    TitleEmpty,
-
-    #[msg("Title is too long, max 50 characters allowed!")]
-    TitleTooLong,
-
     #[msg("Job is not open for applications!")]
     JobNotOpen,
 
@@ -19,6 +10,9 @@ pub enum GaziboError {
 
     #[msg("Job has not been marked as Delivered yet!")]
     JobNotDelivered,
+
+    #[msg("Job can only be cancelled when Open(not after a freelancer accepts)")]
+    JobNotCancellable,
 
     #[msg("Only the assigned freelancer can do this action!")]
     UnauthorizedFreelancer,
@@ -29,9 +23,33 @@ pub enum GaziboError {
     #[msg("No freelancer assigned to this job!")]
     NoFreelancerAssigned,
 
+    #[msg("Escrow accounts has insufficient lamports!")]
+    InsufficientEscrowFunds,
+
+    // Access Control
+    #[msg("Only the job client can perform this action")]
+    NotJobClient,
+
+    #[msg("Only the assigned freelancer can perform this action")]
+    NotAssignedFreelancer,
+
     #[msg("The client can't be the freelancer on their own job!")]
     ClientCannotBeFreelancer,
 
-    #[msg("Escrow accounts has insufficient lamports!")]
-    InsufficientEscrowFunds,
+    // Input Validation
+    #[msg("Payment must be >== 0.01 SOL (1,000,000 Lamports)")]
+    AmountTooLow,
+
+    #[msg("Title can't be empty!")]
+    TitleEmpty,
+
+    #[msg("Title is too long, max 50 characters allowed!")]
+    TitleTooLong,
+
+    #[msg("Description too long, max 500 characters allowed!")]
+    DescriptionTooLong,
+
+    // Arithmetic Safety
+    #[msg("Arithmetic overflow in lamport calculation")]
+    ArithmeticOverflow,
 }
