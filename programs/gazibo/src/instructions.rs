a9ac481023257954b1f8a612d3e3b37e5ac58fff
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-07-02 20:14:22 +0000

    update export instructions

--- programs/gazibo/src/instructions.rs
+++ programs/gazibo/src/instructions.rs
@@ -1,9 +1,11 @@
 pub mod accept_job;
+pub mod cancel_job;
 pub mod create_job;
 pub mod deliver_job;
 pub mod release_payment;
 
 pub use accept_job::*;
+pub use cancel_job::*;
 pub use create_job::*;
 pub use deliver_job::*;
 pub use release_payment::*;
