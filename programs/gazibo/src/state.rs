39433602f7a143d96bc4d901d7256bc13d4020f4
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-07-09 10:08:31 +0000

    add ClientProfile struct and impl it: to create on-chain job counter and avoid namespace collision on the client side

--- programs/gazibo/src/state.rs
+++ programs/gazibo/src/state.rs
@@ -3,20 +3,20 @@ use anchor_lang::prelude::*;
 #[derive(InitSpace)]
 #[account]
 pub struct JobAccount {
-    pub client: Pubkey,
-    pub freelancer: Option<Pubkey>,
-    pub amount: u64,
-    pub status: JobStatus,
+    pub client: Pubkey,             // 32 - job creator
+    pub freelancer: Option<Pubkey>, // 33 - None until accepted (1 tag + 32)
+    pub amount: u64,                // 8 - lamports locked in escrow
+    pub status: JobStatus,          // 1 - current state
 
     #[max_len(50)]
-    pub title: String,
+    pub title: String, // 4 + N bytes (borsh: 4 byte len prefix + content)
 
     #[max_len(500)]
-    pub description: String,
+    pub description: String, // 4 + N bytes
 
-    pub job_id: u64,
-    pub created_at: i64,
-    pub bump: u8,
+    pub job_id: u64,     // 8 - from clientProfile counter (unique per profile)
+    pub created_at: i64, // 8 - unix timestamp
+    pub bump: u8,        // 1 - stored cannonical PDA bump
 }
 
 impl JobAccount {
@@ -25,13 +25,32 @@ impl JobAccount {
         32 + // client: Pubkey
         33 + // freelancer: Option<Pubkey>
         8 + // amount
-        1 + // 
+        1 + // status
         8 + // job_id: u64
         (4 + title_len) + // title: String -> 4 bytes 
         (4 + desc_len) + // description: String -> 4 bytes
         8 + // created_at: u64
         1 //bump
     }
+}
+
+#[account]
+#[derive(Debug)]
+pub struct ClientProfile {
+    pub client: Pubkey,   // 32 - owner wallet
+    pub job_counter: u64, // 8 - next job_id to use (0-indexed)
+    pub total_spent: u64, // 8 - lifetime lamports locked (stats)
+    pub active_jobs: u32, // 4 - currently open/in-progress jobs (stats)
+    pub bump: u8,         // 1 - cannonical bump
+}
+
+impl ClientProfile {
+    pub const SPACE: usize = 8 + // discriminator 
+                                32 + // client
+                                8 + // job_counter
+                                8 + // total_spent
+                                4 + //active jobs
+                                1; // bump
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq, InitSpace)]
