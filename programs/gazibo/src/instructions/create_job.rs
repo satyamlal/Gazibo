b8cc38ef1c3c04820616b9dbc63eeaefdaca49a7
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-07-10 08:56:44 +0000

    add client_profile account in CreateJob struct update job_account and client_profile data with mutable borrow and lexical scoping

--- programs/gazibo/src/instructions/create_job.rs
+++ programs/gazibo/src/instructions/create_job.rs
@@ -4,12 +4,18 @@ use anchor_lang::system_program;
 use crate::constants::*;
 use crate::error::GaziboError;
 use crate::state::{JobAccount, JobStatus};
+use crate::ClientProfile;
 
 #[derive(Accounts)]
 #[instruction(title: String, description: String, job_id: u64)]
 pub struct CreateJob<'info> {
-    #[account(mut)]
-    pub client: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [CLIENT_PROFILE_SEED, client.key().as_ref()],
+        bump = client_profile.bump,
+        has_one = client @ GaziboError::NotJobClient,
+    )]
+    pub client_profile: Account<'info, ClientProfile>,
 
     #[account(
         init,
@@ -23,6 +29,9 @@ pub struct CreateJob<'info> {
         bump
     )]
     pub job_account: Account<'info, JobAccount>,
+
+    #[account(mut)]
+    pub client: Signer<'info>,
 
     pub system_program: Program<'info, System>,
 }
@@ -53,18 +62,38 @@ pub fn create_job_handler(
         amount,
     )?;
 
-    let job = &mut ctx.accounts.job_account;
-    let clock = Clock::get()?;
+    {
+        let job = &mut ctx.accounts.job_account;
+        let clock = Clock::get()?;
 
-    job.client = ctx.accounts.client.key();
-    job.freelancer = None;
-    job.amount = amount;
-    job.status = JobStatus::Open;
-    job.title = title;
-    job.description = description;
-    job.job_id = job_id;
-    job.created_at = clock.unix_timestamp;
-    job.bump = ctx.bumps.job_account;
+        job.client = ctx.accounts.client.key();
+        job.freelancer = None;
+        job.amount = amount;
+        job.status = JobStatus::Open;
+        job.title = title;
+        job.description = description;
+        job.job_id = job_id;
+        job.created_at = clock.unix_timestamp;
+        job.bump = ctx.bumps.job_account;
+    }
+
+    {
+        let client_profile = &mut ctx.accounts.client_profile;
+
+        client_profile.job_counter = job_id
+            .checked_add(1)
+            .ok_or(error!(GaziboError::ArithmeticOverflow))?;
+
+        client_profile.total_spent = client_profile
+            .total_spent
+            .checked_add(amount)
+            .ok_or(error!(GaziboError::ArithmeticOverflow))?;
+
+        client_profile.active_jobs = client_profile
+            .active_jobs
+            .checked_add(1)
+            .ok_or(error!(GaziboError::ArithmeticOverflow))?;
+    }
 
     emit!(JobCreated {
         job_id,
