368e483044b329635a221d5d81bf844d0f0976f5
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-07-23 11:17:48 +0000

    add FREELANCER_PROFILE_SEED GIG_SEED MAX_GIG_TITLE_LEN MAX_METADATA_URI_LEN

--- programs/gazibo/src/constants.rs
+++ programs/gazibo/src/constants.rs
@@ -3,8 +3,12 @@ use anchor_lang::prelude::*;
 #[constant]
 pub const JOB_SEED: &[u8] = b"job";
 pub const CLIENT_PROFILE_SEED: &[u8] = b"client_profile";
+pub const FREELANCER_PROFILE_SEED: &[u8] = b"freelancer_profile";
+pub const GIG_SEED: &[u8] = b"gig";
 
 pub const MAX_TITLE_LENGTH: usize = 50;
 pub const MAX_DESC_LENGTH: usize = 500;
+pub const MAX_GIG_TITLE_LENGTH: usize = 80;
+pub const MAX_METADATA_URI_LEN: usize = 100;
 
 pub const MIN_AMOUNT_LAMPORTS: u64 = 1_000_000; //0.001 SOL = 1_000_000 lamports
