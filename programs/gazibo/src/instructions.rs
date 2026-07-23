d8d3026fa045cc3effb7f5dcbb0619661351fe23
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-07-23 13:40:20 +0000

    update new file paths

--- programs/gazibo/src/instructions.rs
+++ programs/gazibo/src/instructions.rs
@@ -1,13 +1,17 @@
 pub mod accept_job;
 pub mod cancel_job;
+pub mod create_gig;
 pub mod create_job;
 pub mod deliver_job;
 pub mod initialize_client;
+pub mod initialize_freelancer;
 pub mod release_payment;
 
 pub use accept_job::*;
 pub use cancel_job::*;
+pub use create_gig::*;
 pub use create_job::*;
 pub use deliver_job::*;
 pub use initialize_client::*;
+pub use initialize_freelancer::*;
 pub use release_payment::*;
