6b593feeb21b189f5884c6bd274f03b73044bf05
Satyam Lal <l.satyam@op.iitg.ac.in>
2026-08-12 14:10:20 +0000

    update new constants

--- programs/gazibo/src/constants.rs
+++ programs/gazibo/src/constants.rs
@@ -6,9 +6,13 @@ pub const CLIENT_PROFILE_SEED: &[u8] = b"client_profile";
 pub const FREELANCER_PROFILE_SEED: &[u8] = b"freelancer_profile";
 pub const GIG_SEED: &[u8] = b"gig";
 
+pub const ROLE_SEED: &[u8] = b"role_registry";
+
 pub const MAX_TITLE_LENGTH: usize = 50;
 pub const MAX_DESC_LENGTH: usize = 500;
 pub const MAX_GIG_TITLE_LENGTH: usize = 80;
 pub const MAX_METADATA_URI_LEN: usize = 100;
 
 pub const MIN_AMOUNT_LAMPORTS: u64 = 1_000_000; //0.001 SOL = 1_000_000 lamports
+
+pub const PROTOCOL_FEE_BPS: u64 = 0;
